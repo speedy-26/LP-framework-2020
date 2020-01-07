e4efe93be8cead21fe01e9c4d4cedc9b26874119
trannois <harold.trannois@u-picardie.fr>
2020-01-07 10:01:36 +0100

    Exo 3 du TD 0

--- src/Controller/DisqueController.php
+++ src/Controller/DisqueController.php
@@ -1,10 +1,16 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class DisqueController
 {
+    /**
+     * @Route("/disque/info/{ref}", requirements={"ref" = "[A-Z]\d{4}"})
+     * @param $ref
+     * @return Response
+     */
     public function info( $ref ) {
         return new Response(
             "<html><body>".substr($ref,1)."</body></html>"
