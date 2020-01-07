bf7370afadc14fd2686efffbf38ebd2693885ab2
trannois <harold.trannois@u-picardie.fr>
2020-01-07 13:43:29 +0100

    Exo 2 du TD 1 Insére un copyright dans toutes les réponse

--- src/EventListener/FooterEvent.php
+++ src/EventListener/FooterEvent.php
@@ -2,10 +2,16 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\DomCrawler\Crawler;
+
 class FooterEvent
 {
-    public function add()
+    public function add(ResponseEvent $rep)
     {
-
+        $html = $rep->getResponse()->getContent();
+        $crawler = new Crawler($html);
+        $crawler->filter('body')->children()->last()->getNode(0)->appendChild(new \DOMText('©UPJV'));
+        $rep->getResponse()->setContent($crawler->html());
     }
 }
